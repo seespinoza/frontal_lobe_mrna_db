d18d044d22a76e2b02050c466ca5210df220ab82
seespinoza <se.espinoza132@gmail.com>
2020-04-15 23:35:29 -0500

    website

---
+++ flask_website.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/<name>")
+def home(name):
+    return render_template("index.html")
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
